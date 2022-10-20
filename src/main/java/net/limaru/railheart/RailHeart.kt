3851e72020522e3ec82970f824b50ca0cc0ea775
Mineshafter61 <yuhang.061@gmail.com>
2022-10-20 22:08:09 +0900

    Initial Commit

---
+++ src/main/java/net/limaru/railheart/RailHeart.kt
@@ -0,0 +1,13 @@
+package net.limaru.railheart
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RailHeart : JavaPlugin() {
+  override fun onEnable() {
+    // Plugin startup logic
+  }
+
+  override fun onDisable() {
+    // Plugin shutdown logic
+  }
+}
