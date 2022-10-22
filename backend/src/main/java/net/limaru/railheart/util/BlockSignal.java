bd26f6b254fd6db00e44ce745a77fc6aab1e54d6
Mineshafter61 <yuhang.061@gmail.com>
2022-10-22 11:25:56 +0900

    write addTrain

--- backend/src/main/java/net/limaru/railheart/util/BlockSignal.java
+++ backend/src/main/java/net/limaru/railheart/util/BlockSignal.java
@@ -4,54 +4,90 @@ public class BlockSignal {
   // This file uses bitwise programming.
   public long[] track;
   
+  /**
+   * Initialises the object
+   * @param signalCount Total number of signals
+   */
   public BlockSignal(int signalCount) {
     this.track = new long[signalCount>>5+1];  // Split track up into 32-signal sections due to Java's long limit
   }
   
+  /**
+   * Adds a train to the track
+   * @param signalPos Signal to add the train to
+   */
   public void addTrain(int signalPos) {
     // Get section to insert to
-    int i = signalPos / 32;
+    int i = signalPos >> 5;
     // Get position in section
-    signalPos %= 32;
+    signalPos &= 31;
+    // Clear yellow signal
+    this.track[i] ^= 0b01L << (signalPos << 1L);
     // Insert train onto track
-    this.track[i] |= 2L << (signalPos << 1L);
+    this.track[i] |= 0b10L << (signalPos << 1L);
   }
   
-  public void advanceTrains() {
-    long carry = 0L; // Initialise carry
-    for (int i = 0; i < track.length; i++) {
-      // Register carry-over from previous section
-      track[i] |= carry;
-      // Get this section's carry-over for the next section
-      carry = (track[i] & -4611686018427387904L) >>> 62;
-      // Main advance
-      track[i] <<= 2;
-    }
+//  public void advanceTrains() {
+//    long carry = 0L; // Initialise carry
+//    for (int i = 0; i < track.length; i++) {
+//      // Register carry-over from previous section
+//      track[i] |= carry;
+//      // Get this section's carry-over for the next section
+//      carry = (track[i] & -4611686018427387904L) >>> 62;
+//      // Main advance
+//      track[i] <<= 2;
+//    }
+//  }
+  
+  /**
+   * Advances a train by 1 signal
+   * @param signalPos Current signal position
+   */
+  public void advanceTrain(int signalPos) {
+    // Make current position yellow (xx10 -> xx01).
+    this.track[signalPos >> 5] ^= 0b11L << ((signalPos & 31) << 1L);
+    // Make next position red (00xx -> 10xx)
+    this.track[signalPos >> 5] |= 0b10L << ((++signalPos & 31) << 1L);
   }
+  
+  /**
+   * Updates all yellow signal positions
+   */
   public void updateYellows() {
     // Test for reds
-    long[] reds = track.clone();  // copy red signals on track
+    long[] clone = track.clone();  // copy red signals on track
     for (int i = 0; i < track.length; i++) {
-      reds[i] &= 0b10101010_10101010_10101010_10101010_10101010_10101010_10101010_10101010L;
+      clone[i] &= 0b10101010_10101010_10101010_10101010_10101010_10101010_10101010_10101010L;
     }
     // Offset tester array by 2 to check whether the previous signal is also red
     for (int i = 0; i < track.length-1; i++) {
-      reds[i] >>= 2;
-      reds[i] |= (reds[i+1] & 0b10) << 62;
-    } reds[track.length-1] >>= 2;
+      clone[i] >>= 2;
+      clone[i] |= (clone[i+1] & 0b10) << 62;
+    } clone[track.length-1] >>= 2;
     // Remove setter positions that are red
     for (int i = 0; i < track.length; i++) {
-      reds[i] ^= track[i];
+      clone[i] ^= track[i];
     }
     // Insert yellows
     for (int i = 0; i < track.length-1; i++) {
-      reds[i] >>= 1;
-      reds[i] |= (reds[i+1] & 1) << 63;
-      track[i] |= reds[i];
-    } reds[track.length-1] >>= 1;
-    track[track.length-1] |= reds[track.length-1];
+      clone[i] >>= 1;
+      clone[i] |= (clone[i+1] & 1) << 63;
+      track[i] |= clone[i];
+    } clone[track.length-1] >>= 1;
+    track[track.length-1] |= clone[track.length-1];
   }
   
+  /**
+   * Updates all green signal positions
+   */
+  public void updateGreens() {
+  
+  }
+  
+  /**
+   * Displays the object as a human-readable string. Format not handled.
+   * @return A binary representation of the track as a bunch of longs joined together.
+   */
   @Override public String toString() {
     StringBuilder a = new StringBuilder();
     for (long t:track) {
--- backend/src/main/java/net/limaru/railheart/util/BlockSignalTest.java
+++ backend/src/main/java/net/limaru/railheart/util/BlockSignalTest.java
@@ -1,65 +1,31 @@
 package net.limaru.railheart.util;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class BlockSignalTest {
-  private BlockSignal bs0;  // 1 item in track
-  private BlockSignal bs1;  // 2 items in track
-  private BlockSignal bs2;  // 3 items in track
-  private BlockSignal bse;  // edge case
   
-  @org.junit.jupiter.api.BeforeEach
+  @BeforeEach
   void setUp() {
-    bs0 = new BlockSignal(16);
-    bs1 = new BlockSignal(48);
-    bs2 = new BlockSignal(72);
-    bse = new BlockSignal(64);
   }
   
-  @org.junit.jupiter.api.Test
-  void addTrain() {
-    bs0.addTrain(8);  // normal
-    bs0.addTrain(0);  // edge
-    bs0.addTrain(16);  // edge
-  
-    bs1.addTrain(40);  // normal
-    bs1.addTrain(0);  // edge
-    bs1.addTrain(48);  // edge
-  
-    bs2.addTrain(40);  // normal
-    bs2.addTrain(32);  // edge
-    bs2.addTrain(64);  // edge
-    
-    bse.addTrain(14);  // normal
-  }
-  
-  @org.junit.jupiter.api.Test
-  void advanceTrains() {
-    bs0.advanceTrains();
-    bs1.advanceTrains();
-    bs2.advanceTrains();
-    bse.advanceTrains();
-  
-    bs0.advanceTrains();
-    bs1.advanceTrains();
-    bs2.advanceTrains();
-    bse.advanceTrains();  // edge
-  }
-  
-  @org.junit.jupiter.api.Test
-  void updateYellows() {
-    bs0.updateYellows();
-    bs1.updateYellows();
-    bs2.updateYellows();
-    bse.updateYellows();
-  }
-  
-  @org.junit.jupiter.api.AfterEach
+  @AfterEach
   void tearDown() {
-    System.out.println(bs0.toString());
-    System.out.println(bs1.toString());
-    System.out.println(bs2.toString());
-    System.out.println(bse.toString());
+  }
+  
+  @Test
+  void addTrain() {
+  }
+  
+  @Test
+  void advanceTrain() {
+  }
+  
+  @Test
+  void updateYellows() {
   }
 }
